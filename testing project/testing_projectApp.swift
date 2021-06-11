be0913cde4f5572e38cdf628bc9aebb7641912ae
Unthinkable-mac-0040 <unthinkable-mac-0040@Unthinkable-mac-0040s-MacBook-Air.local>
2021-06-11 11:23:18 +0530

    Initial commit

---
+++ testing project/testing_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_projectApp.swift
+//  testing project
+//
+//  Created by Unthinkable-mac-0040 on 11/06/21.
+//
+
+import SwiftUI
+
+@main
+struct testing_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
